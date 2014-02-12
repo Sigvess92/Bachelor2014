db60ef5c2d95493a6392606b7d3e68f29251356c
Sigve <Sigve@10.218.130.168>
2014-02-12 13:12:42 +0100

    db controller

--- src/java/controller/database/executions/DBController.java
+++ src/java/controller/database/executions/DBController.java
@@ -6,11 +6,51 @@
 
 package controller.database.executions;
 
+import controller.database.connections.DBCleaner;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+
 /**
  *
  * @author Sigve
  */
 public class DBController extends controller.database.connections.Establish {
     
+    public DBController(){
+        
+    }
+    
+    public ArrayList<data.WorkHours> getWorkHours(){
+        try {
+            openConnection();
+        } catch (Exception e) {
+            DBCleaner.writeOutput(e, "Could not connect");
+        }
+        ArrayList<data.WorkHours> workHours = new ArrayList<>();
+        String query = "SELECT workhours FROM bach ORDER BY id ASC";
+//        System.out.println(query);
+        PreparedStatement statement01 = null;
+        ResultSet rs = null;
+        try {
+            statement01 = getConnect().prepareStatement(query);
+            rs = statement01.executeQuery();
+            while (rs.next()) {
+//                int autopass_id = rs.getInt("autopass_id");
+                int id = rs.getInt("id");
+                int hoursTotal = rs.getInt("hoursTotal");
+                double percentExternal = rs.getDouble("percentExternal");
+                workHours.add(new data.WorkHours(id, hoursTotal, percentExternal));
+            }
+        } catch (SQLException e) {
+            DBCleaner.writeOutput(e, "AutopassDB finnReisetider(): Error 02");
+        }
+        DBCleaner.closeStatement(statement01);
+        DBCleaner.closeResultSet(rs);
+        closeConnection();
+        return workHours;
+    }
     
 }
